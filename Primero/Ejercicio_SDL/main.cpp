2f831ecf46fa61d2586ac1c7c597918895fbc247
Dust <32524211+dusthandler@users.noreply.github.com>
2018-10-05 11:50:07 +0200

    up

---
+++ Primero/Ejercicio_SDL/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include"SDL/include/SDL.h"
+#pragma comment(lib,"SDL/libx86/SDL2.lib")
+#pragma comment(lib,"SDL/libx86/SDL2main.lib")
+
+using namespace std;
+
+int main(int argc, char*argv[]) {
+
+	cout << "hello world\n";
+
+	system("pause");
+	return 0;
+
+}
